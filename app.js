c7834633693edf84eedade1e4b5d832a12136a58
jooheepark <pjh9641@gmail.com>
2023-11-13 15:28:00 +0900

    feat: POST, PATCH, DELETE 기능 추가

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const fs = require("fs");
 const express = require("express");
-
 const app = express();
 app.use(express.json());
 
-const data = fs.readFileSync("./database.json");
-const conf = JSON.parse(data);
+const database = fs.readFileSync("./database.json");
+const conf = JSON.parse(database);
 
 const { Pool } = require("pg");
 
 // PostgreSQL 연결 정보 설정
-const pool = new Pool({
+const db = new Pool({
   user: conf.user,
   host: conf.host,
   database: conf.database,
@@ -18,42 +17,60 @@ const pool = new Pool({
   port: conf.port,
 });
 
+// article GET
 app.get("/", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM articles");
+    const result = await db.query("SELECT * FROM articles");
     res.json(result.rows);
   } catch (err) {
     console.log(err);
   }
 });
 
-app.post("/articles", (req, res) => {
-  const item = req.body;
-  if (
-    item.cateId &&
-    item.title &&
-    item.createdAt &&
-    item.creator &&
-    item.content
-  ) {
-    pool.query(
-      `INSERT INTO 
-        articles (cate_id, title, created_at, creator, content)
-      VALUES
-        (${item.cateId}, ${item.title}, ${item.createdAt}, ${item.creator}, ${item.content});`,
-      (error, result) => {
-        if (result) {
-          console.log(`Changed Row Count ${result.rowCount}`);
-          res.sendStatus(200);
-        } else {
-          res.sendStatus(500);
-        }
-      }
-    );
+// article POST
+app.post("/articles", async (req, res) => {
+  const data = req.body;
+  if (data.cateId && data.title && data.creator && data.content) {
+    try {
+      const result = await db.query(
+        `INSERT INTO articles (cate_id, title, creator, content) VALUES ($1, $2, $3, $4);`,
+        [data.cateId, data.title, data.creator, data.content]
+      );
+      res.json(result.rows[0]);
+    } catch (err) {
+      console.log(err);
+    }
   } else {
     res.sendStatus(400);
   }
-  // res.send("OK POST~");
+});
+
+// articles PATCH
+app.patch("/articles/:id", async (req, res) => {
+  const articleId = req.params.id;
+  const updatedData = req.body;
+  try {
+    const result = await db.query(
+      `UPDATE articles SET title = $1, content = $2 WHERE id = $3`,
+      [updatedData.title, updatedData.content, articleId]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// articles DELETE
+app.delete("/articles/:id", async (req, res) => {
+  const articleId = req.params.id;
+  try {
+    const result = await db.query(`DELETE FROM articles WHERE id = $1`, [
+      articleId,
+    ]);
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(3000, () => {
