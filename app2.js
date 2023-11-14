39544c853eb35e37823329d10637b488723d0a79
jooheepark <pjh9641@gmail.com>
2023-11-14 17:38:48 +0900

    feat: 덧글 CRUD API 추가

--- app2.js
+++ app2.js
@@ -3,14 +3,33 @@
 const express = require("express");
 const app = express();
 
+let idNum = 1;
+let commentIdNum = 3;
+
+const db = {
+  articles: [
+    {
+      id: 1,
+      cateId: 1,
+      title: "1111",
+      createdAt: "2023-11-14 17:30:30",
+      creator: "박주희",
+      content: "내용",
+      commentsId: [],
+    },
+  ],
+};
+
+const commentDb = {
+  comments: [],
+};
+
 app.set("port", process.env.PORT || 3000);
 app.use(express.json());
 
-let idNum = 1;
-
-const db = {
-  articles: [],
-};
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
+});
 
 app.get("/", (req, res) => {
   res.json("Hello, World");
@@ -146,6 +165,88 @@ app.delete("/articles/:id", (req, res) => {
   }
 });
 
-app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기 중");
+// 덧글 조회
+app.get("/articles/:id/comments", (req, res) => {
+  const articleId = parseInt(req.params.id);
+  const findArticleData = db.articles.find(
+    (article) => article.id === articleId
+  );
+  console.log(findArticleData);
+  if (findArticleData) {
+    const commentsIdArray = findArticleData.commentsId;
+    const commentsData = commentsIdArray?.map((commentId) =>
+      commentDb.comments.find((comment) => comment.id === commentId)
+    );
+    res.json({ articleId: articleId, comments: commentsData });
+  } else {
+    res.status(404).json({ status: 404, message: "존재하지 않는 기사입니다." });
+  }
+});
+
+// 덧글 등록
+app.post("/articles/:id/comments", (req, res) => {
+  const articleId = parseInt(req.params.id);
+  const postData = req.body;
+  const findArticleData = db.articles.find(
+    (article) => article.id === articleId
+  );
+  if (findArticleData) {
+    const newComment = {
+      id: commentIdNum,
+      articleId: articleId,
+      createdAt: postData.createdAt,
+      content: postData.content,
+    };
+    commentDb.comments.push(newComment);
+    findArticleData.commentsId.push(commentIdNum);
+    res.json({
+      message: "덧글이 등록되었습니다.",
+      articleId: articleId,
+      comments: newComment,
+    });
+    commentIdNum += 1;
+  } else {
+    res.status(404).json({ status: 400, message: "존재하지 않는 기사입니다." });
+  }
+});
+
+// 덧글 수정
+app.patch("/articles/:id/comments/:commentId", (req, res) => {
+  const articleId = parseInt(req.params.id);
+  const commentId = parseInt(req.params.commentId);
+  const patchData = req.body;
+  const findCommentData = commentDb.comments.find(
+    (comment) => comment.id === commentId
+  );
+  if (findCommentData) {
+    findCommentData.createdAt = patchData.createdAt;
+    findCommentData.content = patchData.content;
+    res.json({
+      message: "덧글이 수정되었습니다.",
+      articleId: articleId,
+      comments: findCommentData,
+    });
+  } else {
+    res.status(404).json({ status: 404, message: "존재하지 않는 덧글입니다." });
+  }
+});
+
+// 덧글 삭제
+app.delete("/articles/:id/comments/:commentId", (req, res) => {
+  const articleId = parseInt(req.params.id);
+  const commentId = parseInt(req.params.commentId);
+  const findArticleData = db.articles.find(
+    (article) => article.id === articleId
+  );
+  const findCommentIdx = commentDb.comments.findIndex(
+    (comment) => comment.id === commentId
+  );
+  if (findArticleData && findCommentIdx !== -1) {
+    commentDb.comments.splice(findCommentIdx, 1);
+    const deleteCommentIdx = findArticleData.commentsId.findIndex(
+      (comment) => comment === commentId
+    );
+    findArticleData.commentsId.splice(deleteCommentIdx, 1);
+    res.json({ message: "덧글이 삭제되었습니다." });
+  }
 });
